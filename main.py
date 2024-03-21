703a38b2a330c4ba446e4d512414e741ca270bc4
DeepAzz000 <azizsahel18@gmail.com>
2024-03-21 14:41:49 +0000

    Removed Calculations

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from utils import get_links, get_car_characteristics, average_price_per_model, write_to_csv, navigate_to_website
+from logging.config import dictConfig
+from utils import get_links, get_car_characteristics, navigate_to_website , write_to_csv
 
 def main():
+
+    dictConfig(
+        {'version': 1, 'formatters': {'default': {'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s', }},
+        'handlers': {'console': {'class': 'logging.StreamHandler', 'stream': 'ext://sys.stdout', 'formatter': 'default'},
+        'file': {'class': 'logging.FileHandler', 'filename': 'app.log', 'level': 'INFO', 'formatter': 'default'}},
+        'root': {'level': 'INFO', 'handlers': ['console', 'file']}})
     
     try:
         driver = navigate_to_website()
         links = get_links(driver)
         car_data = get_car_characteristics(driver, links)
-        average_price_per_model(car_data)
         write_to_csv(car_data)
     finally:
         driver.quit()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import logging
+from logging.config import dictConfig
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -7,29 +8,34 @@ from selenium.webdriver.support import expected_conditions as EC
 import csv
 from datetime import datetime
 
+
 def navigate_to_website():
-    logging.basicConfig(filename='log_history.log', level=logging.INFO)
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
-    website = 'https://www.avito.ma/fr/maroc/voitures-à_vendre'
-    driver.get(website)
-    logging.info("Navigated to the main page")
-    return driver
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+        website = 'https://www.avito.ma/fr/maroc/voitures-à_vendre'
+        driver.get(website)
+        logging.info("Navigated to the main page")
+        return driver
+    except Exception:
+        logging.info("Couldn't navigate to main element")
 
 def get_links(driver):
-    path_to_box_of_cars = "//*[@id='__next']/div/main/div/div[5]/div[1]/div/div[1]"
-    main_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, path_to_box_of_cars)))
-    logging.info("Main element found")
-
-    tags = main_element.find_elements(By.TAG_NAME, 'a')
-    links = []
-    for tag in tags:
-        link = tag.get_attribute("href")
-        if not link: continue
-        links.append(link)
-    logging.info("Extracted tags")
-    return links
+    try:
+        path_to_box_of_cars = "//*[@id='__next']/div/main/div/div[5]/div[1]/div/div[1]"
+        main_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, path_to_box_of_cars)))
+        logging.info("Main element found")
+        tags = main_element.find_elements(By.TAG_NAME, 'a')
+        links = []
+        for tag in tags:
+            link = tag.get_attribute("href")
+            if not link: continue
+            links.append(link)
+        logging.info("Extracted tags")
+        return links
+    except Exception:
+        logging.info("Could not find main element")
 
 def get_car_characteristics(driver, links):
     logging.info("Getting car elements")
@@ -37,25 +43,28 @@ def get_car_characteristics(driver, links):
     for url in links:
         driver.get(url)        
         car_dict = {}
-    
         main_info = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div/main/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[2]')))
-    
         try:
             car_dict['Name'] = main_info.find_element(By.XPATH, "//*[@id='__next']/div/main/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h1").text
             logging.info("Car name found")
         except:
-            car_dict['Name'] = None
-            logging.info("Car not name found")
+            car_dict['Name'] = 'Not Provided'
+            logging.info("Car name not found")
         try:
             car_dict['Prix'] = main_info.find_element(By.XPATH, "//*[@id='__next']/div/main/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/p").text
+            logging.info("Car Price found")
         except:
-            car_dict['Prix'] = None
+            car_dict['Prix'] = 'Not Provided'
+            logging.info("Car price not found")
         try:
-            car_dict['Localisation'] = main_info.find_element(By.XPATH, "//*[@id='__next']/div/main/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/span[1]").text
+            car_dict['Location'] = main_info.find_element(By.XPATH, "//*[@id='__next']/div/main/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/span[1]").text
+            logging.info("Car Location found")
         except:
-            car_dict['Localisation'] = None
+            car_dict['Location'] = 'Not Provided'
+            logging.info("Car location not found")
         
-        data_types = ['Type', 'Secteur', 'Première main', 'Nombre de portes', 'Modèle', 'Kilométrage', 'Marque', 'Année-Modèle', 'État', 'Origine' ]
+        
+        data_types = ['Scraping_time', 'Type', 'Secteur', 'Première main', 'Nombre de portes', 'Modèle', 'Kilométrage', 'Marque', 'Année-Modèle', 'État', 'Origine' ]
         li_xpath = "//*[@id='__next']/div/main/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[2]/ol/li["
         for datatype in data_types:
             for i in range(1, 10):
@@ -67,49 +76,33 @@ def get_car_characteristics(driver, links):
                         second_span = li_element.find_element(By.XPATH, ".//span[2]")
                         second_span_text = second_span.text
                         car_dict[first_span_text] = second_span_text
+                        logging.info(f'{str(datatype)} found')
+                        car_dict['Scraping_time'] = get_current_timestamp() 
                         break
+                    elif datatype == 'Scraping_time': continue
                     else: 
-                        car_dict[datatype] = None
+                        car_dict[datatype] = 'None'
                 except Exception:
-                    car_dict[datatype] = None
-                
+                    logging.info("looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooool")
+                    car_dict[datatype] = 'None'
         car_data.append(car_dict)
-    logging.info("Car elements found")
+    logging.info("Cars elements found")        
     return car_data
 
-def average_price_per_model(car_data):
-    model_data = {}
-    for car in car_data:
-        model = car['Modèle']
-        price = process_price(car['Prix'])
-        if price is None: continue
-        if model in model_data:
-            model_data[model]['total_price'] += price
-            model_data[model]['count'] += 1
-        else:
-            model_data[model] = {'total_price': price, 'count': 1}
-    for car in car_data:
-        model = car['Modèle']
-        if model in model_data:
-            car['Average Price'] = model_data[model]['total_price'] / model_data[model]['count']
-        else:
-            car['Average Price'] = None
-
-def process_price(price):
-    if price == "PRIX NON SPÉCIFIÉ": return None    
-    price_as_text =  ''.join(char for char in price if char.isdigit())
-    try:
-        price_as_float = float(price_as_text)
-        return price_as_float
-    except Exception as e:
-        logging.info(f"Exception occured while transforming price: {str(e)}")
-        return None
+def get_current_timestamp():
+    return datetime.now().strftime('%Y_%m_%d %H_%M_%S')
 
 def write_to_csv(car_data):
-    timestamp_now_as_text = datetime.now().isoformat().replace(".","_").replace(":","_").replace("-","_")
-    csvfile = open(f'./output_files/avito_data_at_{timestamp_now_as_text}.csv', 'w', newline='', encoding='utf-8')
-    writer = csv.DictWriter(csvfile, fieldnames=car_data[0].keys())
-    writer.writeheader()
-    for car in car_data:
-        writer.writerow(car)
-    csvfile.close()
+    
+    timestamp_now_as_text = get_current_timestamp()
+    try:
+        car_data_file = open(f'./output_files/avito_data_at_{timestamp_now_as_text}.csv', mode='a', newline='', encoding='utf-8')
+        car_data_writer = csv.DictWriter(car_data_file, fieldnames=car_data[0].keys())
+        car_data_writer.writeheader()
+        logging.info(f'Writing to avito_data_at_{timestamp_now_as_text}.csv')
+        for car in car_data:
+            car_data_writer.writerow(car)
+        car_data_file.close()
+        logging.info(f'Writing to avito_data_at_{timestamp_now_as_text}.csv is DONE')
+    except Exception:
+        logging.info("Exception occurred while writing to csv")
